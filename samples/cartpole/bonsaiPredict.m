cf0a17b7ff7946e03db506a5cdbd01f3b7c44ba5
Navvaran Mann <namann@microsoft.com>
2021-02-16 15:26:59 -0800

    update comments

--- samples/cartpole/bonsaiPredict.m
+++ samples/cartpole/bonsaiPredict.m
@@ -2,9 +2,9 @@
 % run in 3 steps:
 %   1. export your brain
 %   2. start exported brain
-%   2. configure the endpoint (defaults to localhost:5000)
-%   3. run this script to configure a predicting session and set required variables
-%   4. open the model and click "Run"
+%   3. configure the endpoint (defaults to localhost:5000)
+%   4. run this script to configure a predicting session and set required variables
+%   5. open the model and click "Run"
 
 mdl = 'cartpole_discrete';
 load_system(mdl);
