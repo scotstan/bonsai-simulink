8ae59ce5ec534c27e511e39afff46e7dbf4ca23f
Navvaran Mann <namann@microsoft.com>
2021-02-16 15:24:15 -0800

    Remove extra parens

--- samples/cartpole/bonsaiConfig.m
+++ samples/cartpole/bonsaiConfig.m
@@ -1,7 +1,7 @@
 % Copyright (c) Microsoft Corporation.
 % Licensed under the MIT License.
 
-function config = bonsaiConfig()
+function config = bonsaiConfig
 
     config = BonsaiConfiguration();
 
