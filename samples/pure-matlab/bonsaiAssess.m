209a46ff4da241744ff7656d7eb012c8f35c77da
Forrest Trepte <forrest@microsoft.com>
2021-04-19 11:05:49 -0700

    better explanation for bonsaiAssess.m

--- samples/pure-matlab/bonsaiAssess.m
+++ samples/pure-matlab/bonsaiAssess.m
@@ -1,5 +1,10 @@
 % Copyright (c) Microsoft Corporation.
 % Licensed under the MIT License.
 
-% The assessment mode (viewing a trained brain running in Simulink)
-% is not supported in this pure MATLAB sample.
+% Other bonsai-simulink samples use bonsaiAssess.m so that a Simulink model
+% can be run inside the MATLAB application to view and troubleshoot
+% execution with a trained brain.
+
+% That approach is not necessary for this pure-matlab example because there
+% is no Simulink model to run. bonsaiTrain.m can be used to register an
+% unmanaged simulator and debug it during either training or assessment.
